2722063242611fd3416d88c0454c5c563b4376e4
aloria <aloria@haworthmedia.com>
2021-04-14 18:04:58 -0500

    completed tests

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -8,11 +8,16 @@ module.exports = class Page {
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open (path) {
-        return browser.url(baseUrl + path)
+        return browser.url(baseUrl + path);
     }
 
-    selectBoolByValue(boolGroup, value) {
-        boolGroup.forEach(element => {
+    /**
+     * Selects a radio button from a group by its value attribute via browser.execute
+     * @param radioGroup parent element containing radio buttons
+     * @param value value attribute of radio button to select
+     */
+    selectRadioByValue(radioGroup, value) {
+        radioGroup.forEach(element => {
             if(element.getValue() == value) {
                 browser.execute('arguments[0].click();', element);
             }
