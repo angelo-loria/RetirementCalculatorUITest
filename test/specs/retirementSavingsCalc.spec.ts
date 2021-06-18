b443de9fb4da9fbf17e2a46890b5d7473294b8b6
Angelo <angelo@Angelos-iMac.local>
2021-06-18 10:09:46 -0500

    Revert "converting to async"
    This reverts commit bb90fc6f363aaa9fe1b3156e06aca26c03ea70af.

--- test/specs/retirementSavingsCalc.spec.ts
+++ test/specs/retirementSavingsCalc.spec.ts
@@ -6,62 +6,52 @@ const RetirementSavingsCalc = require('../pageobjects/retirementSavingsCalc.page
 import * as testData from '../resources/retirementSavingsCalcData.json';
 
 describe('Retirement Savings Calculator should calculate minimum required fields', () => {
-    it('open retirement calculator', async () => {
-        await RetirementSavingsCalc.open();
+    it('open retirement calculator', () => {
+        RetirementSavingsCalc.open();
     });
-    it('set current age', async () =>  {
-        const input = await RetirementSavingsCalc.inputCurrentAge;
-        await input.setValue(testData.CurrentAge);
+    it('set current age', () =>  {
+        RetirementSavingsCalc.inputCurrentAge.setValue(testData.CurrentAge);
     });
-    it('set retirement age', async () => {
-        const input = await RetirementSavingsCalc.inputRetirementAge;
-        await input.setValue(testData.RetirementAge);
+    it('set retirement age', () => {
+        RetirementSavingsCalc.inputRetirementAge.setValue(testData.RetirementAge);
     });
-    it('set current annual income', async () => {
-        const input = await RetirementSavingsCalc.inputCurrentAnnualIncome;
-        await input.setValue(testData.CurrentAnnualIncome);
+    it('set current annual income', () => {
+        RetirementSavingsCalc.inputCurrentAnnualIncome.setValue(testData.CurrentAnnualIncome);
     });
-    it('set current retirement savings', async () => {
-        const input = await RetirementSavingsCalc.inputCurrentTotalSavings;
-        await input.setValue(testData.CurrentRetirementSavings);
+    it('set current retirement savings', () => {
+        RetirementSavingsCalc.inputCurrentTotalSavings.setValue(testData.CurrentRetirementSavings);
     });
-    it('set current annual savings', async () => {
-        const input = await RetirementSavingsCalc.inputCurrentAnnualSavings;
-        await input.setValue(testData.CurrentRetirementContribution);
+    it('set current annual savings', () => {
+        RetirementSavingsCalc.inputCurrentAnnualSavings.setValue(testData.CurrentRetirementContribution);
     });
-    it('set current savings increase rate', async () => {
-        const input = await RetirementSavingsCalc.inputCurrentSavingsIncreaseRate;
-        await input.setValue(testData.AnnualRetirementContribution);
+    it('set current savings increase rate', () => {
+        RetirementSavingsCalc.inputCurrentSavingsIncreaseRate.setValue(testData.AnnualRetirementContribution);
     });
-    it('click Calculate button', async () => {
-        const btn = await RetirementSavingsCalc.btnCalculate;
-        await btn.click();
+    it('click Calculate button', () => {
+        RetirementSavingsCalc.btnCalculate.click();
     });
-    it('verifies results container displayed', async () => {
-        const calcResults = await RetirementSavingsCalc.containerCalculatorResults;
-        await expect(calcResults).toBeDisplayed();
-
-        const resultsChart = await RetirementSavingsCalc.imageResultsChart;
-        await expect(resultsChart).toBeDisplayed();
+    it('verifies results container displayed', () => {
+        expect(RetirementSavingsCalc.containerCalculatorResults).toBeDisplayed();
+        expect(RetirementSavingsCalc.imageResultsChart).toBeDisplayed();
     });       
 });
 
-// describe('Retirement Savings Calculator should display_hide additional SS fields based on SS benefits toggle', () => {
-//     it('open retirement calculator', () => {
-//         RetirementSavingsCalc.open();
-//     });  
-//     it('SS benefits toggle', () => {
-//     RetirementSavingsCalc.selectRadioByValue(
-//         RetirementSavingsCalc.radioBtnsSocialSecurityBenefits, testData.ValueYes);
-//     });
-//     it('verify display additional SS fields', () => {
-//         expect(RetirementSavingsCalc.radioBtnsMaritalStatus).toBeDisplayed()
-//     });
-//     it('SS benefits toggle', () => {
-//         RetirementSavingsCalc.selectRadioByValue(
-//             RetirementSavingsCalc.radioBtnsSocialSecurityBenefits, testData.ValueNo);
-//     });
-//     it('verify not display additional SS fields', () => {
-//         expect(RetirementSavingsCalc.radioBtnsMaritalStatus).not.toBeDisplayed()
-//     });
-// });
+describe('Retirement Savings Calculator should display_hide additional SS fields based on SS benefits toggle', () => {
+    it('open retirement calculator', () => {
+        RetirementSavingsCalc.open();
+    });  
+    it('SS benefits toggle', () => {
+    RetirementSavingsCalc.selectRadioByValue(
+        RetirementSavingsCalc.radioBtnsSocialSecurityBenefits, testData.ValueYes);
+    });
+    it('verify display additional SS fields', () => {
+        expect(RetirementSavingsCalc.radioBtnsMaritalStatus).toBeDisplayed()
+    });
+    it('SS benefits toggle', () => {
+        RetirementSavingsCalc.selectRadioByValue(
+            RetirementSavingsCalc.radioBtnsSocialSecurityBenefits, testData.ValueNo);
+    });
+    it('verify not display additional SS fields', () => {
+        expect(RetirementSavingsCalc.radioBtnsMaritalStatus).not.toBeDisplayed()
+    });
+});
