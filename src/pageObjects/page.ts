7e3f1cd48a9f2d3c4fa5052583c389bb21691ae9
angelo-loria <angelosloria@gmail.com>
2021-07-27 09:43:22 -0500

    add axe audit step

--- src/pageObjects/page.ts
+++ src/pageObjects/page.ts
@@ -1,4 +1,5 @@
 import AxeBuilder from '@axe-core/webdriverjs'
+import AxeCore from 'axe-core'
 
 /**
 * main page object containing all methods, selectors and functionality
@@ -9,9 +10,23 @@ export default class Page {
     * Opens a sub page of the page, appended to baseUrl 'https://www.securian.com'
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    async open (path: string): Promise<void> {
+    async open(path: string): Promise<void> {
         await browser.url(browser.config.baseUrl + path)
     }
-    
 
+    /**
+     * Runs 
+     * @returns AxeCore.AxeResults results of AxeBuilder analysis
+     */
+    async axeAudit(): Promise<AxeCore.AxeResults>{
+        const client: WebdriverIO.Browser = browser
+        const builder: AxeBuilder = new AxeBuilder(client).withTags(['wcag21aa'])
+
+        return await builder.analyze((err, results) => {
+            if (err) {
+              console.log(err)
+            }
+            console.log(results);
+        })
+    }
 }
